b4044fe476230ba345db8703e728fd0438e99181
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-05-11 11:07:42 +0300

    Создание класса, создание экземпляра класса

---
+++ 3-object/ShopProduct.php
@@ -0,0 +1,15 @@
+<?php
+
+// создаем класс
+class ShopProduct
+{
+
+}
+
+// создаем экземляр объекта, создается объект типа ShopProduct
+$product1 = new ShopProduct();
+$product2 = new ShopProduct();
+
+echo '<pre>';
+var_dump($product1);
+var_dump($product2);
