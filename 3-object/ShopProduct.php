0e528b2b45cc0aadc06297ec895b2f5f57c1afac
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-05-11 11:29:41 +0300

    Создание свойств класса

--- 3-object/ShopProduct.php
+++ 3-object/ShopProduct.php
@@ -3,7 +3,10 @@
 // создаем класс
 class ShopProduct
 {
-
+    public $title = "Стандартный товар";
+    public $producerMainName = "Фамилия Автора";
+    public $producerFirstName = "Имя Автора";
+    public $price = 0;
 }
 
 // создаем экземляр объекта, создается объект типа ShopProduct
