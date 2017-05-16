3d1884b1ea0c1528bf6dff896c17a74cfbc6135e
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-05-16 14:31:13 +0300

    Обращение к методу

--- 3-object/ShopProduct.php
+++ 3-object/ShopProduct.php
@@ -7,16 +7,30 @@ class ShopProduct
     public $producerMainName = "Фамилия Автора";
     public $producerFirstName = "Имя Автора";
     public $price = 0;
+
+    // добавляем метод к классу
+    public function getProducer()
+    {
+        return $this->producerFirstName  . " " . $this->producerMainName;
+    }
 }
 
 // создаем экземляр объекта, создается объект типа ShopProduct
 $product1 = new ShopProduct();
 
-// обращение к свойству объекта
-print $product1->title;
 
-// меняем значение свойства
+// меняем значение свойств
 $product1->title = "Собачье сердце";
+$product1->producerMainName = "Булгаков";
+$product1->producerFirstName = "Михаил";
+$product1->price = 5.99;
+
+// обращение к свойствам объекта
+//print "Автор: " . $product1->producerFirstName . ' ' . $product1->producerMainName;
+
+// Обращение к методу
+
+echo "Автор: " . $product1->getProducer();
 
 
 $product2 = new ShopProduct();
