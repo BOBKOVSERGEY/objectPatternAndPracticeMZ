dddb20c3ca347eb3e3e65adaf6569d386dc3229e
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-05-16 14:46:26 +0300

    Вызываем метод __construct

--- 3-object/ShopProduct.php
+++ 3-object/ShopProduct.php
@@ -8,6 +8,15 @@ class ShopProduct
     public $producerFirstName = "Имя Автора";
     public $price = 0;
 
+    // определяем метод конструктора
+    public function __construct($title, $firstName, $mainName, $price)
+    {
+        $this->title = $title;
+        $this->producerMainName = $mainName;
+        $this->producerFirstName = $firstName;
+        $this->price = $price;
+    }
+
     // добавляем метод к классу
     public function getProducer()
     {
@@ -15,26 +24,18 @@ class ShopProduct
     }
 }
 
-// создаем экземляр объекта, создается объект типа ShopProduct
-$product1 = new ShopProduct();
+// вызываем метод констракт
+$product1 = new ShopProduct("Собачье сердце", "Булгаков", "Михаил", 5.99);
 
-
-// меняем значение свойств
-$product1->title = "Собачье сердце";
-$product1->producerMainName = "Булгаков";
-$product1->producerFirstName = "Михаил";
-$product1->price = 5.99;
 
 // обращение к свойствам объекта
 //print "Автор: " . $product1->producerFirstName . ' ' . $product1->producerMainName;
 
 // Обращение к методу
-
 echo "Автор: " . $product1->getProducer();
 
 
-$product2 = new ShopProduct();
+//$product2 = new ShopProduct();
 
 echo '<pre>';
 var_dump($product1);
-var_dump($product2);
